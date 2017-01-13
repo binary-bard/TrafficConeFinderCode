f74ab566614c1aaad4dc3293ce6462ac10a3d7e0
fdou <javatechs@gmail.com>
2017-01-13 11:22:55 -0800

    Simplify setup

--- ROS/cone_finder/setup.py
+++ ROS/cone_finder/setup.py
@@ -3,10 +3,6 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-args = generate_distutils_setup(
-    packages=['cone_finder'],
-    package_dir={'': 'src'},
-    scripts=['scripts']
-    )
+args = generate_distutils_setup(   )
 
 setup(**args)
